44ace244463d35d5716bae7a86258af32576be68
Pizza <wilderpizza99@gmail.com>
2024-05-27 21:16:21 +0200

    added an license

--- src/main/java/com/github/WildePizza/Test.java
+++ src/main/java/com/github/WildePizza/Test.java
@@ -5,6 +5,6 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import java.io.IOException;
 
 class Test {
-    public static void main(String[] args) throws IOException, GitAPIException {
+    public static void main(String[] args) {
     }
 }
